12ab02b44c1129ac307f6fbfba955ec4bd549bfe
ThiruveniS <53971412+ThiruveniS@users.noreply.github.com>
2020-12-06 12:17:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-var bg,ground,mario,brick;
+var bg,marioS,brick;
+var obsI,obs;
+var ground,groundI;
 function preload(){
   marioAn=loadAnimation("mario00.png","mario01.png","mario02.png","mario03.png")
   bgImage=loadImage("bg.png")
+  
+  obsI=loadAnimation("obstacle1.png","obstacle2.png","obstacle3.png","obstacle4.png")
+  
+  groundI=loadImage("ground2.png");
+  
   
 
 }
@@ -11,11 +18,31 @@ function setup(){
   bg=createSprite(450,250,1000,500);
   bg.addAnimation("bg",bgImage)
   bg.scale=1.5;
+  
+  ground=createSprite(450,480,1000,20)
+  ground.addImage("ground",groundI)
     
 }
 function draw(){
   background("lightblue")
-  
+  spawnObstacles();
+  mario();
   drawSprites();
   
+}
+function mario(){
+  marioS=createSprite(50,420,40,40)
+  if(keyDown("space")){
+    marioS.velocityY=-10;
+  }
+  marioS.velocityY=marioS.velocityY+0.5;
+}
+function spawnObstacles(){
+  
+  if(frameCount % 100 === 0) {
+     obstacle = createSprite(1000,420,40,40);
+    obstacle.velocityX=-2;
+    obstacle.addAnimation("obs",obsI)
+    
+}
 }
